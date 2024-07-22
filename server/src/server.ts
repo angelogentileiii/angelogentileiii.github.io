066713c11f4d477d850285fae3ed3e45abea91cc
angelogentileiii <angelo.john.gentile@gmail.com>
2024-07-22 19:27:58 -0400

    host react through express on heroku test

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,8 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import helmet from "helmet";
+import path from "path";
 
 import emailRoutes from "./routes/emailRoutes.js";
 
@@ -11,11 +13,22 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5500;
 
+// Security and parsing middleware
+app.use(helmet());
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// API routes
 app.use("/send-email", verifyRecaptchaToken, emailRoutes);
+
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, "../client/dist")));
+
+// Handle React routing, return the main index.html file
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../client/dist", "index.html"));
+});
 
 const serverRun = async () => {
     try {
