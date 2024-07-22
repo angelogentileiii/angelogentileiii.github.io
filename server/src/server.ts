f60eeea364b94776322dd0606f2a04da94013c63
angelogentileiii <angelo.john.gentile@gmail.com>
2024-07-22 19:19:57 -0400

    update to route for deployment

--- server/src/server.ts
+++ server/src/server.ts
@@ -15,7 +15,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use("/", verifyRecaptchaToken, emailRoutes);
+app.use("/send-email", verifyRecaptchaToken, emailRoutes);
 
 const serverRun = async () => {
     try {
