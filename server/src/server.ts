efec07b5b3b418998bf815af0e89a16bd5ea3f2a
angelogentileiii <angelo.john.gentile@gmail.com>
2024-07-23 12:27:13 -0400

    updates to CSP - frontend and backend

--- server/src/server.ts
+++ server/src/server.ts
@@ -14,11 +14,35 @@ const PORT = process.env.PORT || 5500;
 
 // Security and parsing middleware
 app.use(helmet());
+
+// Configure CSP headers specifically for reCAPTCHA
+app.use(
+    helmet.contentSecurityPolicy({
+        directives: {
+            defaultSrc: ["'self'"],
+            scriptSrc: [
+                "'self'",
+                "https://www.google.com",
+                "https://www.gstatic.com",
+            ],
+            connectSrc: [
+                "'self'",
+                "https://www.google.com",
+                "https://www.gstatic.com",
+            ],
+            // Only include other directives if needed
+        },
+    })
+);
+
+// Manage and control access to resources on your server from different origins
 app.use(cors());
+
+// Build the server
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// API routes
+// Route for email requests
 app.use("/send-email", verifyRecaptchaToken, emailRoutes);
 
 const serverRun = async () => {
